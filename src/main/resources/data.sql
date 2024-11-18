638c971841edad6e134b64a841e6af7916256d0a
Thullyoo <thullyofilho@gmail.com>
2024-11-18 14:32:00 -0300

    feat: add data sql for status

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO TB_STATUS (status_id, description) VALUES (1, 'PENDING');
+INSERT INTO TB_STATUS (status_id, description) VALUES (2, 'SUCCESS');
+INSERT INTO TB_STATUS (status_id, description) VALUES (3, 'CANCELED');
+INSERT INTO TB_STATUS (status_id, description) VALUES (4, 'ERROR');
