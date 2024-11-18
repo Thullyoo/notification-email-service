a8be9b19e1d5f59023b01e0dbc0224c99cb18e81
Thullyoo <thullyofilho@gmail.com>
2024-11-18 18:21:21 -0300

    feat: enable Scheduling in project

--- src/main/java/br/thullyoo/notification_email_service/NotificationEmailServiceApplication.java
+++ src/main/java/br/thullyoo/notification_email_service/NotificationEmailServiceApplication.java
@@ -2,8 +2,10 @@ package br.thullyoo.notification_email_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NotificationEmailServiceApplication {
 
 	public static void main(String[] args) {
