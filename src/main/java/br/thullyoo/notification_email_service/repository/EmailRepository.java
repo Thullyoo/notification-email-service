b74bbfe4f916ecbf1fb3db1d2873c4828f222a65
Thullyoo <thullyofilho@gmail.com>
2024-11-18 18:20:40 -0300

    feat: add query method for check email and send

--- src/main/java/br/thullyoo/notification_email_service/repository/EmailRepository.java
+++ src/main/java/br/thullyoo/notification_email_service/repository/EmailRepository.java
@@ -1,11 +1,15 @@
 package br.thullyoo.notification_email_service.repository;
 
 import br.thullyoo.notification_email_service.entity.email.Email;
+import br.thullyoo.notification_email_service.entity.status.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface EmailRepository extends JpaRepository<Email, UUID> {
+    List<Email> findByStatusInAndDateTimeBefore(List<Status> status, LocalDateTime dateTime);
 }
