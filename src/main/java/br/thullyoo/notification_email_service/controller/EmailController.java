23ab7bb9528840a4fb8cf0233bb34c427ef9d8d3
Thullyoo <thullyofilho@gmail.com>
2024-11-18 17:54:37 -0300

    feat: add endpoint for check status email

--- src/main/java/br/thullyoo/notification_email_service/controller/EmailController.java
+++ src/main/java/br/thullyoo/notification_email_service/controller/EmailController.java
@@ -4,10 +4,9 @@ import br.thullyoo.notification_email_service.entity.email.Email;
 import br.thullyoo.notification_email_service.entity.email.EmailRequest;
 import br.thullyoo.notification_email_service.service.EmailService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/email")
@@ -23,6 +22,12 @@ public class EmailController {
     public ResponseEntity<Email> registerEmail(@RequestBody EmailRequest emailRequest){
             var email = emailService.registerEmail(emailRequest);
             return ResponseEntity.ok().body(email);
+    }
+
+    @GetMapping("/{email_id}")
+    public ResponseEntity<Email> checkEmailStatus(@PathVariable("email_id")UUID email_id){
+        var email = emailService.checkEmailStatus(email_id);
+        return ResponseEntity.ok().body(email);
     }
 
 }
