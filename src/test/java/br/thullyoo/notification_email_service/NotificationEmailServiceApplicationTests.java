346f1a53aad776fce1af9b72a90a914f545f19bc
Thullyoo <thullyofilho@gmail.com>
2024-11-18 13:36:20 -0300

    initial commit

---
+++ src/test/java/br/thullyoo/notification_email_service/NotificationEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.thullyoo.notification_email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationEmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
